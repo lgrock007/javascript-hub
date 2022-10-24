4156d4d2eb75d108c9d0802a60b99f8905088b92
Ramesh M <97971628+lgrock007@users.noreply.github.com>
2022-10-24 23:40:21 +0530

    js - intro

---
+++ script.js
@@ -0,0 +1,24 @@
+/* 
+JavaScript is the world's most popular programming language.
+
+JavaScript is the programming language of the Web.
+
+JavaScript is easy to learn.
+
+*/
+
+// extension is .js
+
+// comment operator is 
+
+// this is comment
+
+/* for 
+a 
+multiple 
+line
+*/
+
+//  JavaScript to program the behavior of web pages
+
+///////////////////////////////////////////////////////////////////
